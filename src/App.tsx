a52f9d4ead81439d8f7fe89a6d31d4046f3cb5eb
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-16 02:42:37 -0400

    Automated deployment test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import './App.css'
 function App() {
 
   return (
+    <>
     <div className='header'>
       What up boi?
     </div>
+    <div>
+      Nothing much boi!
+    </div>
+    </>
   )
 }
 
