0925084d202ea5cc0daad2426250b81aa9da5542
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-16 02:40:38 -0400

    Github actions workflow to deploy app. Builds bundle, deploys infrastructure, pushes bundle to S3, then invalidates cloudfront cache

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -7,4 +7,11 @@ terraform {
       version = "~> 6.45"
     }
   }
+
+  backend "s3" {
+    bucket  = "w-tf-state20260516052247697800000001-433851229368-us-east-2-an"
+    key     = "terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
 }
