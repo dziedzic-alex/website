f1cdd81a55f0af5fafeedcd58379ea164f2fed2c
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-17 19:14:39 -0400

    About me section

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import AboutMe from "./AboutMe.tsx";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <AboutMe />
+    </div>
+  );
+}
+
+export default Home;
